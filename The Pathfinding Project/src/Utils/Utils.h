a8a3fec2cef33411067a203ded82ba34e4aaa447
cristache carol <cristachecarol13@gmail.com>
2023-10-29 17:34:34 +0200

    commit

--- The Pathfinding Project/src/Utils/Utils.h
+++ The Pathfinding Project/src/Utils/Utils.h
@@ -10,13 +10,6 @@
 namespace tpp
 {
 	jclass getClass(JNIEnv* env, const std::string& path);
-	jfieldID getFieldID(JNIEnv* env, const jclass& cl, const char* name, const char* sig);
-	jfieldID getStaticFieldID(JNIEnv* env, const jclass& cl, const char* name, const char* sig);
-	jmethodID getMethodID(JNIEnv* env, const jclass& cl, const char* name, const char* sig);
-	jobject getObject(JNIEnv* env, const jclass& cl, const jfieldID& fieldID);
-	jobject getStaticObject(JNIEnv* env, const jclass& cl, const jfieldID& fieldID);
-	jobject getObject(JNIEnv* env, const jclass& cl, const jclass& fieldIDcl, const char* fieldIDname, const char* fieldIDsig);
-	jobject getStaticObject(JNIEnv* env, const jclass& cl, const jclass& fieldIDcl, const char* fieldIDname, const char* fieldIDsig);
-
+	
 	double clampAngle(double angle, double min, double max);
 }
